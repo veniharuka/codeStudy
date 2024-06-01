94b80d230432c5ef52f76b7d859fbc421aaa80d1
veniharuka <kurobas1384@gmail.com>
2024-06-01 11:06:19 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, is_prefix):
+    answer = 0
+    if len(is_prefix) > len(my_string):
+        return 0
+    for i in range(len(is_prefix)):
+        if is_prefix[i] != my_string[i]:
+            return 0
+    return 1

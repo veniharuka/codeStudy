c5c0a5ca7f20e688e72de3a43e0f89181d452d0a
veniharuka <kurobas1384@gmail.com>
2024-08-28 16:10:14 +0900

    [level 2] Title: 소수 찾기, Time: 30.96 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -28,13 +28,12 @@ class Solution {
         }
     }
     
-    boolean isPrime(int n) {
-        if (n < 2) return false;
-        
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+    boolean isPrime(int n){
+        if(n<2) return false;
+        for(int i=2; i*i<=n; i++){
+            if(n%i==0) return false;
+            
         }
-        
         return true;
     }
 }
